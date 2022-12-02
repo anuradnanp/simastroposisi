377eb2dea3e67fa22fd789cd71c36aac1a47837a
anuradnanp <66927585+anuradnanp@users.noreply.github.com>
2022-12-02 09:22:10 +0700

    Update SimDayInterval.js

--- sims/SimDayInterval.js
+++ sims/SimDayInterval.js
@@ -39,8 +39,8 @@ let DayInterval = function(p) {
 
 	// preloading the fonts
 	p.preload = function() {
-		LatoReg = p.loadFont('../aset/Lato-Regular.ttf');
-		LatoBold = p.loadFont('../aset/Lato-Bold.ttf');
+		LatoReg = p.loadFont('https://anuradnanp.github.io/simastroposisi/aset/Lato-Regular.ttf');
+		LatoBold = p.loadFont('https://anuradnanp.github.io/simastroposisi/aset/Lato-Bold.ttf');
 // 		LatoReg = p.loadFont('file:///android_asset/aset/Lato-Regular.ttf');
 // 		LatoBold = p.loadFont('file:///android_asset/aset/Lato-Bold.ttf'); 
 	};
